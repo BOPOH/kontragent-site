3ade7c05314474bab456eb3d0415911cb5da2c0b
BOPOH <bopoh-a@yandex.ru>
2018-09-08 00:59:10 +0700

    Added user's transactions

---
+++ common/models/Transaction.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "transaction".
+ *
+ * @property int $id
+ * @property int $invoice_id
+ * @property string $stamp
+ * @property int $type
+ * @property string $amount
+ * @property string $balance_after
+ *
+ * @property Invoice $invoice
+ */
+class Transaction extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'transaction';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['invoice_id'], 'required'],
+            [['invoice_id', 'type', 'amount', 'balance_after'], 'default', 'value' => null],
+            [['invoice_id', 'type', 'amount', 'balance_after'], 'integer'],
+            [['stamp'], 'safe'],
+            [['invoice_id'], 'exist', 'skipOnError' => true, 'targetClass' => Invoice::className(), 'targetAttribute' => ['invoice_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'invoice_id' => 'Invoice ID',
+            'stamp' => 'Stamp',
+            'type' => 'Type',
+            'amount' => 'Amount',
+            'balance_after' => 'Balance After',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getInvoice()
+    {
+        return $this->hasOne(Invoice::className(), ['id' => 'invoice_id']);
+    }
+}
---
+++ common/models/TransactionSearch.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Transaction;
+
+/**
+ * TransactionSearch represents the model behind the search form of `common\models\Transaction`.
+ */
+class TransactionSearch extends Transaction
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'invoice_id', 'type', 'amount', 'balance_after'], 'integer'],
+            [['stamp'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Transaction::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'invoice_id' => $this->invoice_id,
+            'stamp' => $this->stamp,
+            'type' => $this->type,
+            'amount' => $this->amount,
+            'balance_after' => $this->balance_after,
+        ]);
+
+        return $dataProvider;
+    }
+}
