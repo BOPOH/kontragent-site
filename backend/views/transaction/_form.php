f82a20064e09795902887033eace26fe49668e3a
BOPOH <bopoh-a@yandex.ru>
2018-09-09 15:28:52 +0700

    Added transactions to the site backend

---
+++ backend/views/transaction/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Transaction */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="transaction-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'invoice_id')->textInput() ?>
+
+    <?= $form->field($model, 'stamp')->textInput() ?>
+
+    <?= $form->field($model, 'type')->textInput() ?>
+
+    <?= $form->field($model, 'amount')->textInput() ?>
+
+    <?= $form->field($model, 'balance_after')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/views/transaction/_search.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\TransactionSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="transaction-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'invoice_id') ?>
+
+    <?= $form->field($model, 'stamp') ?>
+
+    <?= $form->field($model, 'type') ?>
+
+    <?= $form->field($model, 'amount') ?>
+
+    <?php // echo $form->field($model, 'balance_after') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
