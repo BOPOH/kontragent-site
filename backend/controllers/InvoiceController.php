9646c16da97d8923434b7055f4996adbfc1139e6
BOPOH <bopoh-a@yandex.ru>
2018-09-09 15:27:13 +0700

    Added invoices to the site backend

---
+++ backend/controllers/InvoiceController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use common\models\Invoice;
+use common\models\InvoiceSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * InvoiceController implements the CRUD actions for Invoice model.
+ */
+class InvoiceController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [];
+    }
+
+    /**
+     * Lists all Invoice models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new InvoiceSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Invoice model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Finds the Invoice model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Invoice the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Invoice::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
+    }
+}
